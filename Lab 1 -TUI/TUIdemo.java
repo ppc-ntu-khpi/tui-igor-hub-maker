990c05afaca319565737412b4c6c4946a6232f5d
Ihor <>
2024-06-09 15:47:32 +0200

    commit

--- Lab 1 -TUI/TUIdemo.java
+++ Lab 1 -TUI/TUIdemo.java
@@ -7,6 +7,9 @@ import jexer.TText;
 import jexer.TWindow;
 import jexer.event.TMenuEvent;
 import jexer.menu.TMenu;
+import com.mybank.data.DataSource;
+import com.mybank.domain.*;
+import java.util.Locale;
 
 /**
  *
@@ -18,12 +21,18 @@ public class TUIdemo extends TApplication {
     private static final int CUST_INFO = 2010;
 
     public static void main(String[] args) throws Exception {
+        Locale.setDefault(new Locale("en", "US"));
         TUIdemo tdemo = new TUIdemo();
         (new Thread(tdemo)).start();
     }
 
     public TUIdemo() throws Exception {
         super(BackendType.SWING);
+        
+        new DataSource("./data/test.dat").loadData();
+        
+        DataSource ds = new DataSource("./data/test.dat");
+        ds.loadData();
 
         addToolMenu();
         //custom 'File' menu
@@ -49,7 +58,7 @@ public class TUIdemo extends TApplication {
     @Override
     protected boolean onMenu(TMenuEvent menu) {
         if (menu.getId() == ABOUT_APP) {
-            messageBox("About", "\t\t\t\t\t   Just a simple Jexer demo.\n\nCopyright \u00A9 2019 Alexander \'Taurus\' Babich").show();
+            messageBox("About", "\t\t\t\t\t   Just a simple Jexer demo.\n\nCopyright \u00A9 2024 Piatukha Ihor").show();
             return true;
         }
         if (menu.getId() == CUST_INFO) {
@@ -60,6 +69,7 @@ public class TUIdemo extends TApplication {
     }
 
     private void ShowCustomerDetails() {
+        
         TWindow custWin = addWindow("Customer Window", 2, 1, 40, 10, TWindow.NOZOOMBOX);
         custWin.newStatusBar("Enter valid customer number and press Show...");
 
@@ -71,8 +81,19 @@ public class TUIdemo extends TApplication {
             public void DO() {
                 try {
                     int custNum = Integer.parseInt(custNo.getText());
+                    Customer customer = Bank.getCustomer(custNum);
+                    Account account = customer.getAccount(0);
+                    String accountType = "";
+                    
+                    if(account.toString().contains("Saving"))
+                        accountType = "Saving";
+                    else
+                        accountType = "Cheсking";
+                    
+                    String fullNameOfCustomer = customer.getFirstName() + " " + customer.getLastName();
+                    
                     //details about customer with index==custNum
-                    details.setText("Owner Name: John Doe (id="+custNum+")\nAccount Type: 'Checking'\nAccount Balance: $200.00");
+                    details.setText("Owner Name:" + fullNameOfCustomer + "(id=" + custNum + ")\nAccount Type:" + accountType + "\nAccount Balance:" + account.getBalance());
                 } catch (Exception e) {
                     messageBox("Error", "You must provide a valid customer number!").show();
                 }
